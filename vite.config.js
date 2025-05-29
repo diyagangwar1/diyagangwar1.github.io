1469d40b178cdbe5cb7d35cc7982e8aaeff3f18c
Diya Gangwar <diyagangwar1@gmail.com>
2025-05-29 18:23:37 -0500

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import glsl from 'vite-plugin-glsl'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), glsl()],
-  base: '/',
+  base: '/diyagangwar1.github.io/',
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.webp', '**/*.svg'],
   server: {
     hmr: {
